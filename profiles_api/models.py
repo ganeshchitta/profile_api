bd281db69b030b3825fd3f78a71237e219a4c538
ganesh chitta <ganeshchitta90@gmail.com>
2020-11-20 00:12:57 +0530

    User profile models and userprofile model manager creation

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,57 @@
 from django.db import models
-
+# default django user model imports
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """ manager for user profiles"""
+    def create_user(self, email, name, password=None):
+        """ Create a new user profile"""
+        if not email:
+            raise ValueError("users must have email address")
+        
+        # normalize the email address meaning making 2nd half lowercase
+        email = self.normalize_email(email)
+        user =  self.model(email=email, name=name)
+        # sets encrypted password
+        user.set_password(password)
+        # standard syntax for db
+        user.save(using=self._db)
+        return user
+    
+    def create_super_user(self, email, name, password):
+        """ create super user"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+        
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Db model for users in app
+    """
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    # custom model manager for user to control users django knows how to work with userprofile model
+    objects = UserProfileManager()
+    # override default user name with email field for authenticating with email
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+    
+    def get_full_name(self):
+        """ retrieve full name of user"""
+        return self.name
+    
+    def get_short_name(self):
+        """ retrieve short name of user """
+        return self.name
+    
+    def __str__(self):
+        """ return string representation of user (Recommended for django models to convert object to string)"""
+        return self.email 
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.conf.global_settings import AUTH_USER_MODEL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Tells django to use this UserProfile Model by default for authentication
+AUTH_USER_MODEL  = "profiles_api.UserProfile"
