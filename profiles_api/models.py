b3550a8c1f2ad6ae30043c60265b0e2aba76f8e9
ganesh chitta <ganeshchitta90@gmail.com>
2020-11-20 00:22:07 +0530

    created super user  for django admin and registered models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -21,7 +21,7 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """ create super user"""
         user = self.create_user(email, name, password)
         user.is_superuser = True
